2336fa1303eda49d04d1dfe3d66aa2b2105f8ea5
chueh0000 <113487289+chueh0000@users.noreply.github.com>
2026-04-25 01:16:46 +0800

    Update dataset download command allow patterns

--- NuRec/install_nurec_without_sudo.sh
+++ NuRec/install_nurec_without_sudo.sh
@@ -204,8 +204,7 @@ else
     }
     
     echo "Downloading dataset..."
-    # python3 -c "from huggingface_hub import snapshot_download; snapshot_download(repo_id='$DATASET_REPO', repo_type='dataset', local_dir='$DATASET_DIR')" || {
-    python3 -c "from huggingface_hub import snapshot_download; snapshot_download(repo_id='$DATASET_REPO', repo_type='dataset', local_dir='$DATASET_DIR', revision='25.07', allow_patterns='sample_set/25.07_release/Batch0001/026d6a39-bd8f-4175-bc61-fe50ed0403a3/026d6a39-bd8f-4175-bc61-fe50ed0403a3.usdz')" || {
+    python3 -c "from huggingface_hub import snapshot_download; snapshot_download(repo_id='$DATASET_REPO', repo_type='dataset', local_dir='$DATASET_DIR', revision='25.07', allow_patterns='sample_set/25.07_release/*')" || {
         echo "❌ Error: Failed to download the NuRec dataset"
         exit 1
     }
